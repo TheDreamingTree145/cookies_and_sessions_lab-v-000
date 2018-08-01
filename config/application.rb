54f65fde233c707e96080cd4254f374d1db73c63
Dustin Anderson <dandersonsoftware@outlook.com>
2018-08-01 23:24:45 +0000

    automatically backed up by learn

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,12 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module CookiesAndSessionsLab
   class Application < Rails::Application
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
